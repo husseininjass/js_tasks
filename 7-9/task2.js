4389b6a9588da7db298b90be37677849a85c411f
husseininjass <hussein.husam7@gmail.com>
2023-07-09 16:26:04 +0300

    task

--- 7-9/task2.js
+++ 7-9/task2.js
@@ -3,18 +3,18 @@ let Number1 = Number(prompt());
 let Number2 = Number(prompt());
 switch(operations){
     case "*":
-        console.log(Number1 * Number2);
-        document.write(Number1 * Number2);
+        console.log(`${Number1}${operations}${Number2} = ${Number1 * Number2} `);
+        document.write(`${Number1}${operations}${Number2} = ${Number1 * Number2} `);
     break;
     case "+":
-        console.log(Number1 + Number2);
-        document.write(Number1 + Number2);
+        console.log(`${Number1}${operations}${Number2} = ${Number1 + Number2} `);
+        document.write(`${Number1}${operations}${Number2} = ${Number1 + Number2} `);
     break;
     case "/":
-        console.log(Number1/Number2);
-        document.write(Number1/Number2);
+        console.log(`${Number1}${operations}${Number2} = ${Number1 / Number2} `);
+        document.write(`${Number1}${operations}${Number2} = ${Number1 / Number2} `);
     break;
     default:
-        console.log(Number1 - Number2);
-        document.write(Number1 - Number2);
+        console.log(`${Number1}${operations}${Number2} = ${Number1 - Number2} `);
+        document.write(`${Number1}${operations}${Number2} = ${Number1 - Number2} `);
 }
