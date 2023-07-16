33ea89a43a0a648174dff8121c56d24673e84cfd
husseininjass <hussein.husam7@gmail.com>
2023-07-16 16:52:56 +0300

    task

---
+++ 16/7/task1.js
@@ -0,0 +1,30 @@
+//task 1
+// let a = [2,4,6,10];
+// let b = a.map(ele => ele * 2)
+// console.log(b);
+// #####################################################>
+//task2
+// let a = [1,2,3]
+// let b = a.map(ele => ele.toString())
+// console.log(b);
+// #########################################################>
+// task3
+// let a =["itachi" , "madara" , "minato"]
+// let b = a.map(function(ele){
+//     return `${ele[0].toUpperCase()}${ele.slice(1).toLocaleLowerCase()}`;
+// })
+// console.log(b);
+// ##############################################################>
+// task4
+// let a = [5,-4,-6,10];
+// let b = a.map(ele => ele * 2)
+// console.log(b);
+// #################################################################>
+// task5
+// let a = [1,2,3,4]
+// let b = a.map(function(num){
+//     return num * a.indexOf(num)
+// })
+// console.log(b);
+// ##########################################################################>
+
---
+++ 16/7/task2.js
@@ -0,0 +1,13 @@
+//task 1
+// let a = [1,2,3,4];
+// let b = a.forEach((num) => console.log(num*2))
+// #######################################################>
+// task2
+// let a = [1,2,3,4,5,6]
+// let b = a.filter(num => num%2==0)
+// console.log(b);
+// #########################################################>
+// task3
+let a = ["itachi", "goku" , "madara"]
+let b = a.forEach(letter =>console.log(`${letter[0]}${letter[letter.length - 1]}`) )
+
